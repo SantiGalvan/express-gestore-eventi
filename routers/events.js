84e3f14907a7fbfcda9edbe50253a09598cfee5f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 16:28:08 +0200

    created routes reservations

--- routers/events.js
+++ routers/events.js
@@ -9,4 +9,11 @@ router.post("/", eventsController.store);
 // Update
 router.put("/:event", eventsController.update);
 
+// Rotta index reservations
+router.get("/:event/reservations", (req, res) => { });
+// Rotta store reservations
+router.post("/:event/reservations", (req, res) => { });
+// Rotta destroy reservations
+router.delete("/:event/reservations/:reservation", (req, res) => { });
+
 module.exports = router;
