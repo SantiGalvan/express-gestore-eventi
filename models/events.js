32e4882061b834d80a0a9a45102c4548e476aa84
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 14:36:28 +0200

    added getEventId and getEventsTitle

--- models/events.js
+++ models/events.js
@@ -11,17 +11,20 @@ class Event {
         this.maxSeats = maxSeats;
     }
 
+    // Funzione per generare l'id
     generateId() {
         const id = crypto.randomUUID();
         return id;
     }
 
+    // Funzione per leggere il db
     static readDb() {
         const filePath = path.join(__dirname, '../database/db.json');
         const fileData = fs.readFileSync(filePath, 'utf-8');
         return JSON.parse(fileData);
     }
 
+    // Funzione per aggiungere nel db
     static addDb(item) {
 
         const filePath = path.join(__dirname, '../database/db.json');
@@ -32,4 +35,24 @@ class Event {
         fs.writeFileSync(filePath, JSON.stringify(events, null, 2), 'utf-8');
 
     }
+
+    // Filtro per la ricerca tramite id
+    static getEventId(id) {
+        const events = this.readDb();
+        return events.find(event => event.id === id);
+    }
+
+    // Filtro per la ricerca tramite titolo
+    static getEventsTitle(title) {
+        const events = this.readDb();
+
+        const eventsTitle = events.reduce((arr, event) => {
+            event.title === title && arr.push(event);
+            return arr;
+        }, []);
+
+        return eventsTitle;
+    }
 }
+
+module.exports = Event;
