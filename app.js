85fcf3854e12c8868e60047225fa3bf78bf77aba
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 16:20:57 +0200

    imported errors middlewares

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const path = require("path");
+const notFound = require("./middlewares/notfound.js");
+const globalErrors = require("./middlewares/errors.js");
 const port = process.env.PORT || 3000;
 
 // Router degli eventi
@@ -14,6 +16,10 @@ app.get("/", (req, res) => {
 
 // Rotte degli eventi
 app.use('/events', eventsRouter);
+
+// Middleware degli errori
+app.use(notFound);
+app.use(globalErrors);
 
 app.listen(port, () => {
     console.log(`Server avviato sulla porta http://localhost:${port}.`);
