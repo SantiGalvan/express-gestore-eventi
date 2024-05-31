7c6500f5d3cefdad08a53bebc9b4e3c69f2361c7
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 09:57:18 +0200

    added routes index,store and update

--- routers/events.js
+++ routers/events.js
@@ -1,4 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// Index
+router.get("/", (req, res) => { });
+// Store
+router.post("/", (req, res) => { });
+// Update
+router.put("/:event", (req, res) => { });
+
 module.exports = router;
