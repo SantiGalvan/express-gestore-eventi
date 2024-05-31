b72704605755c38402dad061df9784a0997edd40
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 09:43:24 +0200

    started server and added first route

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require("path");
 const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    const filePath = path.join(__dirname, './index.html');
+    res.sendFile(filePath);
+});
+
+app.listen(port, () => {
+    console.log(`Server avviato sulla porta http://localhost:${port}.`);
+})
