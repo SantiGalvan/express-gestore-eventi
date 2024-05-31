35afa6ed5084cde9502a93c3c3134a8fd065c7ba
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 10:01:15 +0200

    added methods index, store and update

---
+++ controllers/events.js
@@ -0,0 +1,15 @@
+
+// Index
+const index = (req, res) => { };
+
+// Store
+const store = (req, res) => { };
+
+// Update
+const update = (req, res) => { };
+
+module.exports = {
+    index,
+    store,
+    update
+}
