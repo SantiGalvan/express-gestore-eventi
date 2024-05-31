1093be27154bd76a44fe9123e405fb6de29fc56c
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 09:52:41 +0200

    imported events router and added routes events

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
 
+// Router degli eventi
+const eventsRouter = require("./routers/events.js");
+
+// Rotta iniziale
 app.get("/", (req, res) => {
     const filePath = path.join(__dirname, './index.html');
     res.sendFile(filePath);
 });
+
+// Rotte degli eventi
+app.use('/events', eventsRouter);
 
 app.listen(port, () => {
     console.log(`Server avviato sulla porta http://localhost:${port}.`);
