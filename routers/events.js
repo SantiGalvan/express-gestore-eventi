4cc5c13a2c74c1cbd581b28f0a5130b52fd1e2f5
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-05-31 10:01:45 +0200

    imported events controller

--- routers/events.js
+++ routers/events.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const eventsController = require("../controllers/events.js");
 
 // Index
-router.get("/", (req, res) => { });
+router.get("/", eventsController.index);
 // Store
-router.post("/", (req, res) => { });
+router.post("/", eventsController.store);
 // Update
-router.put("/:event", (req, res) => { });
+router.put("/:event", eventsController.update);
 
 module.exports = router;
